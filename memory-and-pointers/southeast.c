e1a8d6bccfa75c3d7c96bebd34c69c2b8d2c4387
Goel Biju <goel.biju@students.plymouth.ac.uk>
2019-09-01 22:29:31 +0100

    Changed go_south_east function to use pointers

--- memory-and-pointers/southeast.c
+++ memory-and-pointers/southeast.c
@@ -1,3 +1,37 @@
+/*
+    Using memory pointers:
+
+    1. Get the address of a variable:
+        - Use the & operator to get address:
+            
+            e.g. int x = 4;
+                 printf("x lives at %p\n", &x);
+
+        - Store address in a pointer variable:
+            
+            e.g. int* address_of_x = &x;
+
+    2. Read contents of an address:
+        - When you have memory address, read the data stored there.
+          Use the * operator:
+            
+            e.g. int value_stored = *address_of_x; 
+
+        - * and & are opposites.
+
+        - & operator; takes piece of data and tells you where it's stored.
+
+        - * operator; takes address and tells you what's stored there.
+
+        - Pointers called references, * operator dereferences a pointer.
+
+    3. Change contents of address:
+        - Have pointer variables and change address where variable is pointing,
+          use the * operator, BUT use it on LEFT SIDE of an assignment:
+            
+            e.g. *address_of_x = 99;
+
+*/
 
 #include <stdio.h>
 
@@ -5,14 +39,19 @@
 /*
     The latitude will go down and the longitude increase 
     when moving south-east.
+
+    We pass in the pointers of latitude and longitude.
 */
-void go_south_east(int lat, int lon)
+void go_south_east(int* lat, int* lon)
 {
+    // Use the pointer variable and change the contents 
+    // of both the latitude and longitude variables.
+    
     // Decrease the latitude.
-    lat = lat - 1;
+    *lat = *lat - 1;
 
     // Increase the longitude.
-    lon = lon + 1;
+    *lon = *lon + 1;
 }
 
 
@@ -21,7 +60,8 @@ int main()
     int latitude = 32;
     int longitude = -64;
 
-    go_south_east(latitude, longitude);
+    // Pass in the addresses of latitude and longitude variables.
+    go_south_east(&latitude, &longitude);
 
     printf("Avast! Now at: [%i, %i]\n", latitude, longitude);
 
