27021c3a32173196108040bb8adc65888d891421
Goel Biju <16869075+GoelBiju@users.noreply.github.com>
2019-10-14 10:47:33 +0100

    Update mount.c

--- mount.c
+++ mount.c
@@ -19,9 +19,9 @@ int mount_bind(char* sourceDirectory, char* targetDirectory);
 void unmount(char* targetDirectory);
 
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]) 
 {
-    printf("Mount\n");
+    printf("Opt-Bind-Mount\n");
 
     // Perform the set of actions required:
     // 1. Mount specified location as writable
@@ -37,27 +37,37 @@ int main(int argc, char* argv[])
     // mount_readwrite();
     // mount_bind("", "");
     // unmount("withfiles");
-
+    
 
     // Get the real uid/gid from the user who called it by checking the environmental variables.
-    const char* sudoUid = getenv("SUDO_UID");
-    const char* sudoGid = getenv("SUDO_GID");
+    // const char* sudoUid = getenv("SUDO_UID");
+    // const char* sudoGid = getenv("SUDO_GID");
 
-    if (sudoUid == NULL || sudoGid == NULL)
-    {
-        printf("* Could not get SUDO_UID/SUDO_GID as getenv for SUDO_UID/SUDO_GID returned NULL.\nRun program as sudo?\n");
+    // if (sudoUid == NULL || sudoGid == NULL)
+    // {
+        // printf("* Could not get SUDO_UID/SUDO_GID as getenv for SUDO_UID/SUDO_GID returned NULL.\nRun program as sudo?\n"); 
+        // return -1;
+    // }
+
+    uid_t callingUid = getuid();
+    gid_t callingGid = getgid();
+
+    // callingUid = atoi(sudoUid);
+    // callingGid = atoi(sudoGid);
+
+    printf("True UID (of calling user): %u\n", callingUid);
+    printf("True GID (of calling group): %u\n", callingGid); 
+
+    // Set the uid to root in order to perform the following actions.
+    if (setuid(0) < 0) {
+        printf("* Setting the UID to root (0) did not work, are you sure the executable has the setuid-bit set?\n");
+
         return -1;
     }
 
-    uid_t callingUid;
-    gid_t callingGid;
+    printf("- Set UID to root (0): %u\n", getuid());
 
-    callingUid = atoi(sudoUid);
-    callingGid = atoi(sudoGid);
-
-    //printf("True UID (of calling user): %u\n", callingUid);
-    //printf("True GID (of calling group): %u\n", callingGid);
-
+    return 0;
 
     // Handle the arguments that are passed to the program.
     int i;
@@ -66,11 +76,11 @@ int main(int argc, char* argv[])
         printf("Number of arguments supplied: %d\n", argc - 1);
 
         // TODO: Needs specific arguments e.g. -p (mount point), -f (folder name), -l (mount location).
-        // Get the arguments needed and their values; remove mount bind with umount (-u argument?).
+        // Get the arguments needed and their values; remove mount bind with umount (-u argument?).	
         //for (i = 1; i < argc; i++)
         //{
-        //    char* argument = argv[i];
-        //    printf("Argument: %s\n", argument);
+            // char* argument = argv[i];
+            // printf("Argument: %s\n", argument);
         //}
 
         char* mountPoint = argv[1];
@@ -82,12 +92,13 @@ int main(int argc, char* argv[])
 
         // Mount specified location as writable.
         int res;
-
+        
         res = mount_readwrite(mountPoint);
         if (res < 0)
         {
-             printf("* Failed to set %s as read/write, result: %d\nRun program as sudo?\n", mountPoint, res);
-             return -1;
+            printf("* Failed to set %s as read/write, result: %d\nRun program as sudo?\n", mountPoint, res); 
+
+            return -1;
         }
 
         printf("Successfully set %s as read/write.\n", mountPoint);
@@ -97,7 +108,7 @@ int main(int argc, char* argv[])
         res = mkdir(mountLocation, 0755);
         if (res <  0)
         {
-            printf("* Failed to create directory at following path: %s\n", mountLocation);
+            printf("* Failed to create directory at following path: %s\n", mountLocation); 
             mount_readonly(mountPoint);
 
             return -1;
@@ -112,57 +123,57 @@ int main(int argc, char* argv[])
 
         if (tmpDirName == NULL)
         {
-            printf("* Failed to create temporary directory with mkdtemp.\n");
+        printf("* Failed to create temporary directory with mkdtemp.\n");
             mount_readonly(mountPoint);
-
-            return -1;
+        
+        return -1;
         }
-
+		
         printf("Created tmp directory name: %s\n", tmpDirName);
-        // TODO: Delete this temporary directory after use.
+	    // TODO: Delete this temporary directory after use. 
 
-        // Use the real uid and gid to change the ownership of the directory.
+
+	    // Use the real uid and gid to change the ownership of the directory.
         if (chown(tmpDirName, callingUid, callingGid) == -1)
-        {
+	    {
             printf("* Failed to change the ownership of the temporary directory (/tmp/%s) to %d (uid): %d (gid).\n", tmpDirName, callingUid, callingGid);
             return -1;
-        }
+	    }
 
         printf("Successfully changed ownership of the temporary directory (/tmp/%s) to %d (uid): %d (gid).\n", tmpDirName, callingUid, callingGid);
 
 
-        // Re-mount location as read-only.
+	    // Re-mount location as read-only.
         res = mount_readonly(mountPoint);
-        if (res < 0)
-        {
-             printf("* Failed to set %s as read-only.\n", mountPoint);
-             return -1;
-        }
+	    if (res < 0)
+	    {
+            printf("* Failed to set %s as read-only.\n", mountPoint);
+            return -1;	     
+	    }
 
-        printf("Successfully set %s as read-only.\n", mountPoint);
+	    printf("Successfully set %s as read-only.\n", mountPoint);
 
 
-        // Bind mount the temporary location with the mount location.
+	    // Bind mount the temporary location with the mount location.
         res = mount_bind(tmpDirName, mountLocation);
-        if (res < 0)
-        {
+	    if (res < 0)
+	    {
             printf("* Failed to bind mount %s to %s, result: %d\n", tmpDirName, mountLocation, res);
-            return -1;
-        }
+	        return -1;
+	    }
 
         printf("Successfully bind mounted %s with %s.\n", tmpDirName, mountLocation);
     }
-    else if (argc < 3)
+    else if (argc < 3) 
     {
-        printf("* Please provide the mount point and the mount location (including the directory to create), in that order.\nE.g. ./mount /opt /opt/software/cmake\n");
+        printf("* Please provide the mount point and the mount location (including the directory to create), in that order.\n");
         return -1;
     }
-    else
+    else 
     {
         printf("* No arguments passed to the program.\n");
-        return -1;
+    	return -1;
     }
-
 
     printf("- Operation complete.\n");
     return 0;
@@ -177,7 +188,7 @@ int mount_readonly(char* mountPoint)
     int result = mount("", mountPoint, fileType, MS_REMOUNT | MS_RDONLY, "");
     printf("- Re-mounting result: %d\n", result);
 
-    return result;
+    return result;    
 }
 
 
@@ -185,7 +196,7 @@ int mount_readwrite(char* mountPoint)
 {
     char* fileType = "nfs4";
 
-    printf("- Re-mounting %s as read and writable (MS_REMOUNT) with file type: %s\n", mountPoint, fileType);
+    printf("- Re-mounting %s as read and writable (MS_REMOUNT) with file type: %s\n", mountPoint, fileType);  
     int result = mount("", mountPoint, fileType, MS_REMOUNT, "");
     printf("- Re-mounting result: %d\n", result);
 
@@ -193,10 +204,10 @@ int mount_readwrite(char* mountPoint)
 }
 
 
-int mount_bind(char* sourceDirectory, char* targetDirectory)
+int mount_bind(char* sourceDirectory, char* targetDirectory) 
 {
     char* fileType = "nfs4";
-
+   
     printf("- Bind mounting %s to %s (MS_BIND) with file type: %s\n", sourceDirectory, targetDirectory, fileType);
     int result = mount(sourceDirectory, targetDirectory, fileType, MS_BIND, "");
     printf("- Bind Mounting result: %d\n", result);
@@ -210,3 +221,5 @@ void unmount(char* targetDirectory)
     int result = umount(targetDirectory);
     printf("Unmounting result: %d\n", result);
 }
+
+
